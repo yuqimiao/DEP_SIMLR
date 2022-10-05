ca0d286647793230d7aa1dd8596c6ed52cfed8f9
yuqimiao <ym2771@cumc.columbia.edu>
2022-10-05 19:51:09 -0400

    simu_220930 analysis

---
+++ code/simu_220930_analysis.R
@@ -0,0 +1,130 @@
+# mds plot for simulation result
+cur_id = 304
+sim_res = tib_all %>%
+  filter(noise_sd == 2) %>%
+  filter(simu_id == cur_id) %>%
+  unnest(res_tib)
+
+part_S_kernel = (sim_res %>% filter(kernel == "kernel" & method == "part_cimlr") %>% pull(res_list))[[1]]$S
+part_S_diff_kernel = (sim_res %>% filter(kernel == "diff_kernel" & method == "part_cimlr") %>% pull(res_list))[[1]]$S
+
+part_S_kernel_cl = (sim_res %>% filter(kernel == "kernel" & method == "part_cimlr") %>% pull(res_list))[[1]]$cluster
+part_S_diff_kernel_cl = (sim_res %>% filter(kernel == "diff_kernel" & method == "part_cimlr") %>% pull(res_list))[[1]]$cluster
+
+source("code/functions/Partition_CIMLR_2.0.R")
+part_S_kernel_dist = dist_kernels(part_S_kernel)
+part_S_diff_kernel_dist = dist_kernels(part_S_diff_kernel)
+
+source("code/functions/visualization_functions.R")
+# Labeled by the cluster result
+mds_kernel = as_tibble(cmdscale(part_S_kernel_dist, 2))%>%
+  mutate(cluster = part_S_kernel_cl) %>%
+  ggplot(aes(x = V1, y = V2, color = factor(cluster))) +
+  geom_point()+
+  labs(title = "MDS plot of dist(YY^T) from part-cimlr-kernel ",
+       color = "cluster label")
+
+mds_diff_kernel = as_tibble(cmdscale(part_S_diff_kernel_dist, 2))%>%
+  mutate(cluster = part_S_diff_kernel_cl) %>%
+  ggplot(aes(x = V1, y = V2, color = factor(cluster))) +
+  geom_point()+
+  labs(title = "MDS plot of dist(YY^T) from part-cimlr-diff_kernel ",
+       color = "cluster label")
+
+# Labeled by the cluster truth
+mds_kernel_t = as_tibble(cmdscale(part_S_kernel_dist, 2))%>%
+  mutate(cluster = rep(1:4, each = 50)) %>%
+  ggplot(aes(x = V1, y = V2, color = factor(cluster))) +
+  geom_point()+
+  labs(title = "MDS plot of dist(YY^T) from part-cimlr-kernel ",
+       color = "truth")
+
+mds_diff_kernel_t = as_tibble(cmdscale(part_S_diff_kernel_dist, 2))%>%
+  mutate(cluster = rep(1:4, each = 50)) %>%
+  ggplot(aes(x = V1, y = V2, color = factor(cluster))) +
+  geom_point()+
+  labs(title = "MDS plot of dist(YY^T) from part-cimlr-diff_kernel ",
+       color = "truth")
+
+(mds_kernel|mds_diff_kernel)/
+(mds_kernel_t|mds_diff_kernel_t)
+
+# check the kernels and diff_kernels ----
+library(tidyverse)
+library(igraph)
+# simulation for 4 clusters, data 1 separates 1/2A/2B, data 2 separates 1A/1B/2 ----
+# output:
+# data matrix with n_sub*n_feat
+
+get_data_4clust = function(n_sub = 200,
+                           n_feat = 1000,
+                           s2n_ratio = 0.05, # signal to noise ratio
+                           mu_vec = c(1, 2, 10, 20), # feature mean for 4 clusters
+                           signal_sd = 1,
+                           noise_sd = 1){
+  data = matrix(0, n_sub, n_feat)
+  n_signal = floor(n_feat*s2n_ratio)
+  signal_ind = 1:n_signal
+  noise_ind = (n_signal+1):n_feat
+
+
+  n_sub_perc = n_sub/4
+  cluster_index = lapply(1:4, function(i) ((i-1)*n_sub_perc+1):(i*n_sub_perc))
+
+  # noise simulation
+  data[1:n_sub, noise_ind]=rnorm(n = n_sub*length(noise_ind), mean = 0, sd = noise_sd)
+
+
+  # signal_simulation
+  for(i in 1:4){
+    cluster_index_cur = cluster_index[[i]]
+    mu_cur = mu_vec[i]
+    data[cluster_index_cur, signal_ind]=rnorm(n = n_sub_perc*length(signal_ind), mean = mu_cur, sd = signal_sd)
+  }
+
+  return(data)
+}
+
+# get kernel and diff kernel ----
+
+
+noise_sd_all = seq(1,3,0.5)
+
+## simulate
+n_feat1 = 1000
+n_feat2 = 100000
+mu1 = c(0, 0, 1, -1)
+mu2 = c(1,-1,0,0)
+noise_sd = 3
+
+data1 = get_data_4clust(n_feat = n_feat1, mu_vec = mu1, noise_sd = noise_sd)
+data2 = get_data_4clust(n_feat = n_feat2, mu_vec = mu2, noise_sd = noise_sd)
+
+data_list = list(data1, data2)
+
+## benchmark
+alpha = 1
+sigma = 2 # kernel_parameter
+diffusion_form = "L1"
+n = nrow(data_list[[1]])
+k = floor(sqrt(n))
+source("code/functions/Partition_CIMLR_2.0.R")
+source("code/functions/CIMLR_kernel_input.R")
+
+distance_list = lapply(data_list, function(x) dist2(x,x))
+kernel_list = lapply(distance_list, function(x) kernel_calculation(distance = x, k = k, sigma = sigma ))
+diff_kernel_list = lapply(kernel_list, function(x) diffusion_enhancement(kernel = x, alpha = alpha, k = k , diffusion_form = diffusion_form))
+S = kernel_list[[1]]
+S0 = S
+diag(S0) = mean(S0)
+heatmap_gg(S0,"kernel")
+
+source("code/functions/visualization_functions.R")
+L = normalized_GL(S)
+estimateNumberOfClustersGivenGraph(S,2:5)
+plot((n-10):(n-1), eigen(L)$values[(n-10):(n-1)])
+heatmap_gg(eigen(L)$vectors[,(n-5):n],"GL 1-5 smallest eigenvec")
+compare(kmeans(eigen(S)$vectors[,2],3)$cluster, c(rep(1,100), rep(2:3, each = 50)), "nmi")
+
+
+
